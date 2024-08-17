53f59fcf7acb7c95cef4c77130d9aa21ba585117
Tyson <tysonjerico123@gmail.com>
2024-08-17 12:53:45 -0400

    This is the first version

---
+++ CSC320_Module_1_CTA/src/PrintOuts_to_system.java
@@ -0,0 +1,18 @@
+package src;
+
+	public class PrintOuts_to_system {
+
+		public static void main(String[] args) {
+			//This code prints out the first name to the console
+			System.out.println ("John");
+			// This code prints out the last name to the console
+			System.out.println ("Jones");
+			//This code prints out the street address to the console
+			System.out.println ("432 Reetes Rd.");
+			//This code prints out the city to the console
+			System.out.println ("Yutil, CO.");
+			//This code prints out the zip code to the console
+			System.out.println ("80016.");
+	}
+
+}
